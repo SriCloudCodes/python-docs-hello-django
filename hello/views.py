9c4fd23c3c7cfb2738aac635a08cef74eae8ecd5
SriCloudCodes <84981260+SriCloudCodes@users.noreply.github.com>
2021-07-16 11:35:18 +0530

    Update views.py
    New updates

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!, Success Srinivasan!")
+    return HttpResponse("Hello, World!, Success Srinivasan!, Great Job Today!!! ")
